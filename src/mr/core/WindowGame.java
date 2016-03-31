ef417b9b55807d89d1448d451d1679a22f98df3b
simon.pierre <sh.pierre@laposte.net>
2016-03-31 18:43:04 +0200

    Added lvl load

--- src/mr/core/WindowGame.java
+++ src/mr/core/WindowGame.java
@@ -6,11 +6,18 @@ import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
 
+import mr.controller.LevelLoader;
+import mr.core.exception.InputFileNotFoundException;
+import mr.model.Level;
 import mr.view.Renderer;
+import mr.view.RenderingContext;
 import mr.view.ResourceHandler;
+import mr.view.screen.ScreenRenderer;
 
 public class WindowGame extends BasicGame {
 	private Renderer renderer;
+	
+	private RenderingContext context;
 	
     public WindowGame() {
         super("Core :: WindowGame");
@@ -22,10 +29,21 @@ public class WindowGame extends BasicGame {
     	ResourceHandler.init();
     	
     	this.renderer = new Renderer();
+    	this.context = new RenderingContext(null);
+    	String level = "level.lvl.txt";
+        try {
+			Level lvl = LevelLoader.loadLevel(level);
+			
+			ScreenRenderer.addScreenToContext(context, lvl.getStartingScreen(), lvl);
+		} catch (InputFileNotFoundException e) {
+			System.err.println("Cannot load level : "+level);
+			e.printStackTrace();
+		}
     }
 
     @Override
     public void render(GameContainer container, Graphics g) throws SlickException {
+    	this.renderer.update(context);
     	this.renderer.render(g);
     }
 
@@ -35,7 +53,7 @@ public class WindowGame extends BasicGame {
     }
     
     public static void main(String[] args) throws SlickException {
-        new AppGameContainer(new WindowGame(), 640, 480, false).start();
+        new AppGameContainer(new WindowGame(), 640, 640, false).start();
     }
 }
 
