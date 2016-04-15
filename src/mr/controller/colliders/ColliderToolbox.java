70ef0f7f9318dff9d35c38773713e1e6b9fb09cc
Simon PIERRE <newsafeaddiction@gmail.com>
2016-04-15 20:55:39 +0200

    Added : HitBox
    Added : handling of HitBox in ColliderToolbox

--- src/mr/controller/colliders/ColliderToolbox.java
+++ src/mr/controller/colliders/ColliderToolbox.java
@@ -4,12 +4,30 @@ import mr.model.misc.Coordinate;
 
 public class ColliderToolbox {
 
+	public static boolean collide(Coordinate posA, HitBox hitBoxA, Coordinate posB, HitBox hitBoxB) {
+		return isInside(
+				posA.x+hitBoxA.offset.x,
+				posA.x+hitBoxA.offset.x+hitBoxA.size.x,
+				posA.y+hitBoxA.offset.y,
+				posA.y+hitBoxA.offset.y+hitBoxA.size.y,
+				posB.x+hitBoxB.offset.x,
+				posB.x+hitBoxB.offset.x+hitBoxB.size.x,
+				posB.y+hitBoxB.offset.y,
+				posB.y+hitBoxB.offset.y+hitBoxB.size.y
+				);
+	}
 
 	public static boolean isInside(float leftX,float rightX,float upY,float downY, Coordinate pos, Coordinate size) {
-		if ( isInside(leftX,rightX,upY,downY,pos.x,pos.y)
-				|| isInside(leftX,rightX,upY,downY,pos.x+size.x,pos.y)
-				|| isInside(leftX,rightX,upY,downY,pos.x+size.x,pos.y+pos.y)
-				|| isInside(leftX,rightX,upY,downY,pos.x,pos.y) ) {
+		return isInside(leftX, rightX, upY, downY, pos.x, pos.x+size.x,pos.y,pos.y+size.y);
+	}
+
+	public static boolean isInside(float leftX,float rightX,float upY,float downY,
+			float leftXB,float rightXB,float upYB,float downYB) {
+
+		if ( isInside(leftX,rightX,upY,downY,leftXB,upYB)
+				|| isInside(leftX,rightX,upY,downY,rightXB,upYB)
+				|| isInside(leftX,rightX,upY,downY,rightXB,downYB)
+				|| isInside(leftX,rightX,upY,downY,leftXB,downYB) ) {
 			return true;
 		}
 		return false;
---
+++ src/mr/controller/colliders/HitBox.java
@@ -0,0 +1,8 @@
+package mr.controller.colliders;
+
+import mr.model.misc.Coordinate;
+
+public class HitBox {
+	public Coordinate offset;
+	public Coordinate size;
+}
