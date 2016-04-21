8a353791d6ec53b980669855f6404d63c6f87f67
Simon PIERRE <newsafeaddiction@gmail.com>
2016-04-21 21:31:29 +0200

    Imrpoved : model now handles size and hitbox

--- src/mr/model/model/Model.java
+++ src/mr/model/model/Model.java
@@ -1,8 +1,19 @@
 package mr.model.model;
 
+import mr.model.misc.Coordinate;
+
 public class Model {
 	private String id;
 	private String sprite;
+	private Coordinate size;
+	private Coordinate hitBoxOffset;
+	private Coordinate hitBoxSize;
+
+	public Model() {
+		this.size = new Coordinate();
+		this.hitBoxOffset = new Coordinate();
+		this.hitBoxSize = new Coordinate();
+	}
 
 	public String getId() {
 		return id;
@@ -15,5 +26,23 @@ public class Model {
 	}
 	public void setSprite(String sprite) {
 		this.sprite = sprite;
+	}
+	public Coordinate getSize() {
+		return size;
+	}
+	public void setSize(Coordinate size) {
+		this.size = size;
+	}
+	public Coordinate getHitBoxOffset() {
+		return hitBoxOffset;
+	}
+	public void setHitBoxOffset(Coordinate hitBoxOffset) {
+		this.hitBoxOffset = hitBoxOffset;
+	}
+	public Coordinate getHitBoxSize() {
+		return hitBoxSize;
+	}
+	public void setHitBoxSize(Coordinate hitBoxSize) {
+		this.hitBoxSize = hitBoxSize;
 	}
 }
--- test/mr/controller/ModelHandlerTest.java
+++ test/mr/controller/ModelHandlerTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 
 import mr.core.exception.FormatModelException;
 import mr.core.exception.InputFileNotFoundException;
+import mr.model.GameConstant;
 import mr.model.model.AIModel;
 import mr.model.model.Model;
 import mr.model.model.ProjectileModel;
@@ -13,18 +14,24 @@ public class ModelHandlerTest {
 
 	@Test
 	public void test() throws InputFileNotFoundException, FormatModelException {
-		ModelHandler.get().load("resources/models.data.txt");
+		ModelHandler.get().load("resources/TESTmodels.data.txt");
 
 		Object[][] expecteds = new Object[][] {
-			{"model1", "model1.png"},
-			{"model2", "model2.png"},
-			{"model3", "model3.png"}
+			{"model1", "model1.png", 1.f, 2.f, 3.f, 4.f, 5.f, 6.f},
+			{"model2", "model2.png", 6.f, 5.f, 4.f, 3.f, 2.f, 1.f},
+			{"model3", "model3.png", 9.f, 8.f, 7.f, 6.f, 5.f, 4.f}
 		};
 
 		for ( Object[] expected : expecteds ) {
 			Model model = ModelHandler.get().getModel((String) expected[0]);
 			Assert.assertEquals(expected[0], model.getId());
 			Assert.assertEquals(expected[1], model.getSprite());
+			Assert.assertEquals((float)expected[2], model.getSize().x,GameConstant.epsilon);
+			Assert.assertEquals((float)expected[3], model.getSize().y,GameConstant.epsilon);
+			Assert.assertEquals((float)expected[4], model.getHitBoxOffset().x,GameConstant.epsilon);
+			Assert.assertEquals((float)expected[5], model.getHitBoxOffset().y,GameConstant.epsilon);
+			Assert.assertEquals((float)expected[6], model.getHitBoxSize().x,GameConstant.epsilon);
+			Assert.assertEquals((float)expected[7], model.getHitBoxSize().y,GameConstant.epsilon);
 		}
 
 		expecteds = new Object[][] {
@@ -36,7 +43,7 @@ public class ModelHandlerTest {
 			ProjectileModel model = ModelHandler.get().getProjectileModel((String) expected[0]);
 			Assert.assertEquals(expected[0], model.getId());
 			Assert.assertEquals(expected[1], model.getSprite());
-			Assert.assertEquals((float)expected[2], model.getSpeed(),0.0001f);
+			Assert.assertEquals((float)expected[2], model.getSpeed(),GameConstant.epsilon);
 			Assert.assertEquals(expected[3], model.getDamage());
 		}
 
@@ -48,7 +55,7 @@ public class ModelHandlerTest {
 			AIModel model = ModelHandler.get().getAIModel((String) expected[0]);
 			Assert.assertEquals(expected[0], model.getId());
 			Assert.assertEquals(expected[1], model.getSprite());
-			Assert.assertEquals((float)expected[2], model.getSpeed(),0.0001f);
+			Assert.assertEquals((float)expected[2], model.getSpeed(),GameConstant.epsilon);
 			Assert.assertEquals(expected[3], model.getLife());
 			Assert.assertEquals(expected[4], model.getProjectileModel());
 		}
